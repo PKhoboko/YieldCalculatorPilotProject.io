da1b22d1848bb8af6529ddb27a2f7a1bb1ef0305
Pitso Khoboko <88023611+PKhoboko@users.noreply.github.com>
2025-02-03 23:39:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,13 +57,12 @@ def dashboard():
         # Take the first 3% of the data
         subset_data = data[:three_percent_rows]
         for i in subset_data:
-            
+            value = value.replace(",", ".")
             if isinstance(i['Ears'], (int, float)) and isinstance(i['Grain mass'].replace(",", "."), (int, float)) \
-               and isinstance(i['Moist %'].replace(",", "."), (int, float)) and isinstance(i['Row width'].replace(",", "."), (int, float)):
-              i["yeild"] = jsonify((int(i['Ears'])*(float(i['Grain mass'])*((100-float(i['Moist %']))/(100-12.5)/float(i['Row width']))))*0.95)
+            and isinstance(i['Moist %'].replace(",", "."), (int, float)) and isinstance(i['Row width'].replace(",", "."), (int, float)):
+                data["yeild"] = jsonify((int(i['Ears'])*(float(i['Grain mass'])*((100-float(i['Moist %']))/(100-12.5)/float(i['Row width']))))*0.95)
             else:
-               i["yeild"] = 0
-       
+                data["yeild"] = 0
         
         return render_template('dashboard.html',data=subset_data)
     total_rows = len(data)  # Get total number of rows
@@ -72,12 +71,11 @@ def dashboard():
         # Take the first 3% of the data
     subset_data = data[:three_percent_rows]
     for i in data:
-	ear = 
-        if isinstance(str(i['Ears'])), (int, float)) and isinstance(str(i['Grain mass']).replace(",", "."), (int, float)) \
-        and isinstance(str(i['Moist %']).replace(",", "."), (int, float)) and isinstance(str(i['Row width']).replace(",", "."), (int, float)):
-            i["yeild"] = jsonify((int(str(i['Ears']).replace(",", "."))*(float(str(i['Grain mass']).replace(",", "."))*((100-float(str(i['Moist %']).replace(",", ".")))/(100-12.5)/float(str(i['Row width']).replace(",", ".")))))*0.95)
+        if isinstance(i['Ears'], (int, float)) and isinstance(i['Grain mass'].replace(",", "."), (int, float)) \
+        and isinstance(i['Moist %'].replace(",", "."), (int, float)) and isinstance(i['Row width'].replace(",", "."), (int, float)):
+            data["yeild"] = jsonify((int(i['Ears'])*(float(i['Grain mass'])*((100-float(i['Moist %']))/(100-12.5)/float(i['Row width']))))*0.95)
         else:
-            i["yeild"] = 0
+            data["yeild"] = 0
             
     return render_template('dashboard.html',data=data)
 
