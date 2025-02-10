2aea588a040a4f389f20afa197d2daa16b295361
Pitso Khoboko <88023611+PKhoboko@users.noreply.github.com>
2025-02-10 20:35:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from supabase_config import supabase
 from supabase import create_client, Client
+from flask_storage import FlaskSessionStorage
 
 
 
